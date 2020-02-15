55649c60e8a1977e6fc04cb5f873a39fe27cae0a
marcingrabowski <magr@danskenet.net>
2020-02-15 14:56:27 +0100

    Initial commit

---
+++ app/src/main/java/eu/gitcode/songapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package eu.gitcode.songapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.gitcode.songapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}

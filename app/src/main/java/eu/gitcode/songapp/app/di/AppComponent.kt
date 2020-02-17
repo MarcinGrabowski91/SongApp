828a3160b56ebdc12748dbb7a61ae4a0b13b29ae
marcingrabowski <magr@danskenet.net>
2020-02-18 01:11:42 +0200

    Dependencies cleanup

--- app/src/main/java/eu/gitcode/songapp/app/di/AppComponent.kt
+++ app/src/main/java/eu/gitcode/songapp/app/di/AppComponent.kt
@@ -1,7 +1,6 @@
 package eu.gitcode.songapp.app.di
 
 import dagger.Component
-import dagger.android.support.AndroidSupportInjectionModule
 import eu.gitcode.core.di.CoreComponent
 import eu.gitcode.core.di.scope.AppScope
 import eu.gitcode.songapp.app.App
@@ -9,7 +8,6 @@ import eu.gitcode.songapp.app.App
 @AppScope
 @Component(
     modules = [
-        AndroidSupportInjectionModule::class,
         AppModule::class
     ],
     dependencies = [CoreComponent::class]

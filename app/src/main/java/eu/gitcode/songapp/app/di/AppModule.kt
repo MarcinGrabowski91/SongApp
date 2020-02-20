874be41f446eeb07cb1128e1a95fc5e93f7bd178
marcingrabowski <magr@danskenet.net>
2020-02-20 15:57:33 +0100

    ViewModel factory duplication removed

--- app/src/main/java/eu/gitcode/songapp/app/di/AppModule.kt
+++ app/src/main/java/eu/gitcode/songapp/app/di/AppModule.kt
@@ -1,28 +1,13 @@
 package eu.gitcode.songapp.app.di
 
 import android.content.Context
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import dagger.Module
 import dagger.Provides
 import eu.gitcode.songapp.app.App
-import javax.inject.Provider
-import javax.inject.Singleton
 
 @Module
 class AppModule {
 
     @Provides
     fun provideContext(application: App): Context = application.applicationContext
-
-    @Suppress("UNCHECKED_CAST")
-    @Provides
-    @Singleton
-    fun provideViewModelFactory(
-        providers: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
-    ) = object : ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return requireNotNull(providers[modelClass as Class<out ViewModel>]).get() as T
-        }
-    }
 }
